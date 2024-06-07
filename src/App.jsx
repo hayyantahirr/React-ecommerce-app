ec741b74ecb79dd16874095df980660a377fcb72
hayyantahirr <hayyantahir7@gmail.com>
2024-06-08 02:42:30 +0500

    ecommerce app completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import Card from "./components/Card";
+import axios from "axios";
 
 function App() {
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    axios("https://dummyjson.com/products")
+      .then((res) => {
+        setProduct(res.data.products);
+        console.log(product);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
   return (
     <>
-
-
-    
       <Navbar />
 
-      <div className="flex flex-wrap justify-center gap-7 mt-16">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </div>
+      {
+        <div className="flex flex-wrap justify-center gap-7 mt-16">
+          {product ? (
+            product.map((item) => {
+              return (
+                <Card
+                  key={item.id}
+                  title={item.title}
+                  description={item.description}
+                  img={item.thumbnail}
+                  price={item.price}
+                />
+              );
+            })
+          ) : (
+            <h1>
+              <span className="loading loading-spinner loading-sm"></span>
+            </h1>
+          )}
+        </div>
+      }
     </>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 
-function Card() {
+function Card(prop) {
   return (
     <>
       <div className="card w-96 bg-base-100 shadow-xl image-full">
         <figure>
           <img
-            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+            src={prop.img}
             alt="Shoes"
           />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <h2 className="card-title">{prop.title}</h2>
+          <p>{prop.description}</p>
           <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+            <button className="btn btn-primary">${prop.price}</button>
           </div>
         </div>
       </div>
